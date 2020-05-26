bbaf609ec117e351ed4cc7b60491722612b45d34
Alexandra Raibolt <raibolt@ime.eb.br>
2020-05-26 15:29:52 -0300

    Initial commit globals.py

---
+++ globals.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+def initialize():
+	global K
+	global data_test_images
+	global data_train_images
+	global descriptor
+	global detector
+	global file
+	global num_classes
+	global test_feature_vec
+	global train_feature_vec
